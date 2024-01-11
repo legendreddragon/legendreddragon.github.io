ae589aed58c7e251032bda6d3c7b550e3b6aa0e1
Tavinus <guneves@gmail.com>
2024-01-11 05:00:47 -0300

    sort sort sort

--- assets/js/sortable.js
+++ assets/js/sortable.js
@@ -149,6 +149,9 @@ function functionAddAttribute(){
     document.querySelector(".armours th:nth-child(2)").setAttribute('aria-sort', 'ascending');
     document.querySelector(".weaponsarmours th:nth-child(2)").setAttribute('aria-sort', 'ascending');
     document.querySelector(".masters th:nth-child(2)").setAttribute('aria-sort', 'ascending');
-    document.querySelectorAll(".monsters th:nth-child(4)").setAttribute('aria-sort', 'ascending');
+    //document.querySelectorAll(".monsters th:nth-child(4)").setAttribute('aria-sort', 'ascending');
+    document.querySelectorAll(".monsters th:nth-child(4)").forEach(e => {
+        e.setAttribute('aria-sort', 'ascending');
+    });
 };
 window.onload = functionAddAttribute;
