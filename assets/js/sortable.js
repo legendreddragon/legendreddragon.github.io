fd479db7cb9f7168f6e5b4afe60d98fa230f87f4
Tavinus <guneves@gmail.com>
2024-01-18 20:13:17 -0300

    'Wiki Update'

--- assets/js/sortable.js
+++ assets/js/sortable.js
@@ -394,9 +394,10 @@ Date.prototype.addHours = function (hours) {
 //var d = new Date(0); // epoch
 //var servertimeseconds = parseInt(document.getElementById("ServerTime").value);
 //d.setUTCSeconds(servertimeseconds);
-var serverTime = (new Date()).addHours(3);
+var serverTime = (new Date()).addHours(-3);
 var serverTime = (new Date()).getTime();
 var timeDiff = (new Date()).getTime() - serverTime;
+console.log(timeDiff);
 var displaytime = new Date();
 var lastTime = "";
 
